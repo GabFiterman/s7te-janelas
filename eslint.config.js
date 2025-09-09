7776f9a4041330386de90578a199b992d1136bbf
GabFiterman <gfiterman96@gmail.com>
2025-09-09 18:49:21 -0300

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import prettier from 'eslint-plugin-prettier'
+import tseslint from 'typescript-eslint'
+import { globalIgnores } from 'eslint/config'
+
+export default tseslint.config(
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'warn',
+    },
+  },
+  {
+    files: ['**/*.{js,ts,tsx}'],
+    rules: {
+      ...prettier.configs.recommended.rules,
+    },
+  },
+)

99591ad5cf3115e7b954ad26a37cd4f3553666ac
GabFiterman <gfiterman96@gmail.com>
2025-09-17 11:00:20 -0300

    feature/issue-2 patch: add icons to fixed menu - adjust menu style

--- src/components/fixed-menu/fixed-menu.tsx
+++ src/components/fixed-menu/fixed-menu.tsx
@@ -2,15 +2,33 @@ import useFixedMenuStates from './use-fixed-menu';
 import './fixed-menu.scss';
 
 function FixedMenu() {
-    const { menuItems } = useFixedMenuStates();
+    const { menuItems, mainItem } = useFixedMenuStates();
     return (
         <>
             <div className="fixed-menu">
-                <img
-                    src={menuItems[0].icon}
-                    alt={menuItems[0].label}
-                    className="main-icon"
-                />
+                <div className="main-icon-container">
+                    <img
+                        src={mainItem.icon}
+                        alt={mainItem.label}
+                        className="main-icon"
+                        onClick={mainItem.action}
+                    />
+                </div>
+                <div className="menu-items-container">
+                    {menuItems.map((item) => (
+                        <div className="menu-item" key={item.id}>
+                            <img
+                                src={item.icon}
+                                alt={item.label}
+                                className="menu-icon"
+                                onClick={item.action}
+                            />
+                        </div>
+                    ))}
+                </div>
+
+                {/* TODO: Adicionar componente container de widgets aqui */}
+                <div className="menu-widgets-container">widgets</div>
             </div>
         </>
     );
--- src/components/fixed-menu/use-fixed-menu.ts
+++ src/components/fixed-menu/use-fixed-menu.ts
@@ -1,16 +1,39 @@
 import windowIcon from '@/assets/logo/windows-logo.png';
+import internetExplorerIcon from '@/assets/icons/internet_explorer.webp';
+import pictureIcon from '@/assets/icons/picture.webp';
+import fileExplorerIcon from '@/assets/icons/file_explorer.webp';
 
 function useFixedMenuStates() {
+    const mainItem = {
+        id: 1,
+        label: 'FloatMenu',
+        action: () => console.log('FloatMenu'),
+        icon: windowIcon,
+    };
+
     const menuItems = [
         {
-            id: 1,
-            label: 'FloatMenu',
-            link: '/float-menu',
-            icon: windowIcon,
+            id: 2,
+            label: 'Internet Explorer',
+            action: () => console.log('Internet Explorer'),
+            icon: internetExplorerIcon,
+        },
+        {
+            id: 3,
+            label: 'File Explorer',
+            action: () => console.log('File Explorer'),
+            icon: fileExplorerIcon,
+        },
+        {
+            id: 4,
+            label: 'Picture',
+            action: () => console.log('Picture'),
+            icon: pictureIcon,
         },
     ];
     return {
         menuItems,
+        mainItem,
     };
 }
 

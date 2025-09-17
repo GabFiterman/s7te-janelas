02b60ff7d0aaa0421f42150c1d1c2d39354eb105
GabFiterman <gfiterman96@gmail.com>
2025-09-17 17:28:24 -0300

    feature/issue-2 chore: add new Icons

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,11 @@
+export { default as blocksIcon } from './blocks.webp';
+export { default as cdPlayerIcon } from './cd_player.webp';
+export { default as computerColorsIcon } from './computer_colors.webp';
+export { default as fileExplorerIcon } from './file_explorer.webp';
+export { default as folderIcon } from './folder-1.webp';
+export { default as gameHubIcon } from './game_hub.webp';
+export { default as indicationArrowIcon } from './indication_arrow.svg';
+export { default as internetExplorerIcon } from './internet_explorer.webp';
+export { default as pictureIcon } from './picture.webp';
+export { default as sheetMusicIcon } from './sheet_music.webp';
+export { default as usersIcon } from './users.webp';

0c086b03bf3b750ce7bc7ecc46f4993c01c49be1
GabFiterman <gfiterman96@gmail.com>
2025-10-15 17:01:39 -0300

    feature/2 feat: add new component BtnIconTextLink

---
+++ src/components/common/buttons/btn-icon-text-link/btn-icon-text-link.tsx
@@ -0,0 +1,24 @@
+import { type JSX } from 'react';
+
+import './btn-icon-text-link.scss';
+
+interface BtnIconTextLinkProps {
+  text: string;
+  icon: string | JSX.Element;
+  className?: string;
+  style?: React.CSSProperties;
+  iconSize?: string;
+}
+
+function BtnIconTextLink({ text, icon, className, style, iconSize }: BtnIconTextLinkProps) {
+  return (
+    <button className={`btn-icon-text-link ${className ? className : ''}`} style={style}>
+      <picture className="icon">
+        {typeof icon === 'string' ? <img src={icon} alt={text} style={{ width: iconSize, height: iconSize }} /> : icon}
+      </picture>
+      <span className="text">{text}</span>
+    </button>
+  );
+}
+
+export default BtnIconTextLink;
--- src/components/common/buttons/index.ts
+++ src/components/common/buttons/index.ts
@@ -1,1 +1,2 @@
 export { default as BtnForwardBackward } from './btn-forward-backward/btn-forward-backward';
+export { default as BtnIconTextLink } from './btn-icon-text-link/btn-icon-text-link';

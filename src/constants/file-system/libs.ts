c61ff30ae5272115e3d6f7b55099dc3d86c98ba6
GabFiterman <gfiterman96@gmail.com>
2025-11-11 15:00:36 -0300

    feature/11 refactor: file-system-map

---
+++ src/constants/file-system/libs.ts
@@ -0,0 +1,9 @@
+import { type FileSystemItem } from '../file-system-map';
+import { ITEMS_MAP_ALL } from './all-items';
+
+export const ITEMS_MAP_LIBS: FileSystemItem[] = [
+  ITEMS_MAP_ALL['C:/USUARIOS/FITERMAN/DOCUMENTOS'],
+  ITEMS_MAP_ALL['C:/USUARIOS/FITERMAN/IMAGENS'],
+  ITEMS_MAP_ALL['C:/USUARIOS/FITERMAN/MÚSICAS'],
+  ITEMS_MAP_ALL['C:/USUARIOS/FITERMAN/VIDEOS'],
+];
